0435d030867ef51ff972a0878b7eaba32af2d4d1
Sean Conrad <seanconrad123@gmail.com>
2018-05-31 22:45:05 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ['Honda', 'Toyota', 'Tesla', 'Chevrolet', 'Ford']
+  array = ['Honda', 'Toyota', 'Tesla', 'Chevrolet']
 end
 
 def add_element_to_end_of_array(array, element)
